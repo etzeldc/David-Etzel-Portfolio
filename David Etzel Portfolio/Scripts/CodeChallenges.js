b496a6d61426b178fb12f4c88255120003cb796a
etzeldc <etzeldc@gmail.com>
2019-07-15 16:55:33 -0400

    Style Collision Repairs
    Everything except the modal header.

--- David Etzel Portfolio/Scripts/CodeChallenges.js
+++ David Etzel Portfolio/Scripts/CodeChallenges.js
@@ -1,11 +1,11 @@
 ﻿//Code Challenges
 
 $(function () {
-    $(".code").hide();
+    $(".codeStuff").hide();
 
     //Calculations for Five Numbers
-    var a, b, c, d, e;
     $("#fiveCalc").click(function () {
+        var a, b, c, d, e;
         a = +$("#five1").val();
         b = +$("#five2").val();
         c = +$("#five3").val();
@@ -21,13 +21,13 @@ $(function () {
         $("#sumNum").text("The SUM of the numbers entered is " + sum + ".");
         var prod = a * b * c * d * e;
         $("#prodNum").text("The PRODUCT of the numbers entered is " + prod + ".");
-    })
+    });
 
     //Clearing all Five Number Input and Output Fields
     $("#fiveClear").click(function () {
         $(".input").val("");
         $(".output").text("");
-    })
+    });
 
 
     //Calculations for Factorals
@@ -38,13 +38,13 @@ $(function () {
             temp = temp * i;
         }
         $("#factOut").text("The Factoral of " + value + " is " + temp + ".");
-    })
+    });
 
     //Clearing Factoral Input and Output Fields
     $("#factClear").click(function () {
         $("#factIn").val("");
         $(".output").text("");
-    })
+    });
 
     //Calculations for FizzBuzz
     $("#fbCalc").click(function () {
@@ -52,25 +52,25 @@ $(function () {
         var buzz = $("#buzz").val();
         var fizzBuzz = [];
         for (i = 1; i <= 100; i++) {
-            if (i % fizz == 0 && i % buzz == 0) {
+            if (i % fizz === 0 && i % buzz === 0) {
                 fizzBuzz.push("<span class='fb shared'>FizzBuzz</span>");
-            } else if (i % fizz == 0) {
+            } else if (i % fizz === 0) {
                 fizzBuzz.push("<span class='b shared'>Buzz</span>");
-            } else if (i % buzz == 0) {
+            } else if (i % buzz === 0) {
                 fizzBuzz.push("<span class='f shared'>Fizz</span>");
             } else {
                 fizzBuzz.push(i);
             }
         }
         $("#fbOut").html(fizzBuzz.join(', '));
-    })
+    });
 
 
     //Clearing FizzBuzz Input and Output Fields
     $("#fbClear").click(function () {
         $("#fizz, #buzz").val("");
         $("#fbOut").text("");
-    })
+    });
 
     //Calculations for Palindrome
     $("#palCalc").click(function () {
@@ -88,13 +88,13 @@ $(function () {
             $("#palOut").text("The reverse of \"" + word + "\" is \"" + word + "\".");
             $("#palResult").text("\"" + word + "\" is a palindrome.");
         }
-    })
+    });
 
     //Clearing Palindrome input and output fields
     $("#palClear").click(function () {
         $("#palIn").val("");
         $("#palOut, #palResult").text("");
-    })
+    });
 
     //Calculations for Sum Of All Fears
     $("#kCalc").click(function () {
@@ -104,7 +104,7 @@ $(function () {
         var kTrue = false;
         for (var i = 0; i < fear.length - 1; i++) {
             for (var j = i + 1; j <= fear.length - 1; j++) {
-                if (fear[i] + fear[j] == k) {
+                if (fear[i] + fear[j] === k) {
                     kTrue = true;
                     $("#kResponse").text("K has been found!");
                     $("#kEquation").text(fear[i] + " + " + fear[j] + " = " + k);
@@ -115,22 +115,22 @@ $(function () {
         if (!kTrue) {
             $("#kResponse").text("K was not found in this array.");
         }
-    })
+    });
 
     //Clearing Sum Of All Fears Input and Output Fields
     $("#kClear").click(function () {
         $(".input").val("");
         $(".output").text("");
-    })
+    });
 
     //"Show/Hide Code" Button Operations
     $(".btnToggle").click(function () {
-        $(".code").toggle();
-        if ($(this).text() == "Show Code") {
+        $(".codeStuff").toggle();
+        if ($(this).text() === "Show Code") {
             $(this).text("Hide Code");
         } else {
             $(this).text("Show Code");
         }
-    })
+    });
 });
 
